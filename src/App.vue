00e7b7642f480041f8b1683b7f7fcfb1de6892b9
Elizaveta Kulinich <kulinich.eliz@gmail.com>
2023-04-01 19:21:05 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,83 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+      <form>
+        <h4>Create post</h4>
+        <input v-bind:value="title" @input="inputTitle" class="input" type="text" placeholder="Название">
+        <input v-bind:value="body" class="input" type="text" placeholder="Описание">
+        <button class="btn" @click="createPost">Create</button>
+
+      </form>
+   <div class="post" v-for="post in posts">
+    <div><strong>{{ post.title }}</strong></div>
+    <div><strong>Описание</strong>{{ post.body }}</div>
+   </div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import { eventNames } from 'process';
+
+export default{
+  data(){
+    return {
+      posts:[
+        {id:1, title:"JavaScript", body:"Описание поста"},
+        {id:2, title:"JavaScript", body:"Описание поста2"},
+        {id:3, title:"JavaScript", body:"Описание поста3"},
+      ],
+      title:"",
+      body:"",
+    }
+  },
+  methods:{
+   createPost(){
+
+   },
+   inputTitle(event){
+    this.title = event.target.value;
+   }
+  }
+
 }
 
-nav {
-  padding: 30px;
+</script>
+
+<style >
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.app{
+  padding: 20px;
+}
+.post{
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+form{
+  display: flex;
+  flex-direction: column;
 }
+
+.input{
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn{
+  align-self: flex-end;
+  margin-top: 15px;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+
+
 </style>
